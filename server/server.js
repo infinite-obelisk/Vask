cd045e1feaad65a906f341433920640b07437c18
Bryan Jennings <bryanjenningz@gmail.com>
2015-05-22 12:15:30 -0700

    Fix server.js app.get

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/../client/'));
-app.get('/', function(request, response) {
-  response.sendFile('app.js');
-});
 
-app.listen(3000, function() {
-  console.log('Listening on 3000');
+// This isn't necessary right now because express.static automatically
+// searches for the index.html file in the specified directory that was 
+// passed in as an argument.
+// app.get('/', function(request, response) {
+//   console.log('APP GET');
+//   response.sendFile(__dirname + '/../client/index.html');
+// });
+
+var port = 80;
+app.listen(port, function() {
+  console.log('Listening on ' + port);
 });
