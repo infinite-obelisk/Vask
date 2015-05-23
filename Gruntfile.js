d1be5e6f53b3f508ccd8a6481a949da3f951b0e2
Kirstin "Kir" Jarchow <me@kirstinjarchow.com>
2015-05-23 15:18:29 -0700

    Move files. Add less compiling.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['client/build/*', 'server/*']
+      files: ['uccviews/jsx/*', 'server/*']
     },
     watch: {
       files: ['client/*.html', 'client/build/*', 'client/views/*', 'server/*', 'test/*'],
@@ -12,13 +12,20 @@ module.exports = function(grunt) {
     exec: {
       reactify: {
         //Will have to add a browserify for each. Gross, I know -Kir
-        command: "browserify -t reactify jsx/lectureView.js -o client/build/lectureView.js"
+        command: "browserify -t reactify uccviews/jsx/lectureView.js -o client/build/js/lectureView.js"
       }
     },
     uglify: {
       my_target: {
         files: {
-          'client/build/lectureView.min.js': ['client/build/lectureView.js']
+          'client/build/js/lectureView.min.js': ['client/build/js/lectureView.js']
+        }
+      }
+    },
+    less: {
+      dist: {
+        files: {
+          "client/build/css/material-ui.css": "uccviews/less/material-ui.less"
         }
       }
     }
@@ -27,9 +34,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-exec');
 
   grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('reactifies', ['exec', 'uglify']);
+  grunt.registerTask('reactifies', ['less', 'exec', 'uglify']);
 
 };
--- uccviews/jsx/lectureView.js
+++ uccviews/jsx/lectureView.js
@@ -1,8 +1,17 @@
+var injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 var mui = require('material-ui');
 
 var permanentsrc = "eyU3bRy2x44";
 
-var YoutubeVideo = React.createClass({
+var YoutubeVideo = React.createClass(
+{
   render: function(){
     return <iframe id="youtube" width="560" height="315" src={"https://www.youtube.com/embed/" + this.props.ytsrc} frameborder="0" allowfullscreen></iframe>;
   }
