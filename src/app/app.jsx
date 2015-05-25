e10ef90e94b72952dd6fdf9dd52356ceb792821a
Elvio Cavalcante <elvio@madfx.com.br>
2015-05-25 15:24:57 -0700

    Gulp is now compiling jsx files with material design. Package.json has more dependencies and .gitignore file contains the build directory.

---
+++ src/app/app.jsx
@@ -0,0 +1,20 @@
+(function () {
+  var React = require('react/addons');
+  var injectTapEventPlugin = require('react-tap-event-plugin');
+  var Main = require('./components/main.jsx'); // Our custom react component
+  var PRODUCTS = require('./data.js');
+
+  //Needed for React Developer Tools
+  window.React = React;
+
+  //Needed for onTouchTap
+  //Can go away when react 1.0 release
+  //Check this repo:
+  //https://github.com/zilverline/react-tap-event-plugin
+  injectTapEventPlugin();
+
+  // Render the main app react component into the document body. 
+  // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
+  React.render(<Main  products={PRODUCTS}/>, document.body);
+
+})();
