8db0be17538d944a4a34c590abd8e9e4c83dafe4
Kirstin "Kir" Jarchow <me@kirstinjarchow.com>
2015-05-25 17:14:23 -0700

    Add template files

---
+++ uccviews/jsx/template.js
@@ -0,0 +1,58 @@
+var injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+var React = require('react'),
+    mui = require('material-ui'),
+    AppBar = mui.AppBar,
+    IconButton = mui.IconButton,
+    FontIcon = mui.FontIcon;
+var ThemeManager = new mui.Styles.ThemeManager();
+ThemeManager.setTheme(ThemeManager.types.LIGHT);
+
+var NavBar = React.createClass({
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+  getChildContext:function() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    }
+  },
+  render: function(){
+    return (<AppBar title="Vask" />);
+  }
+});
+
+// CUSTOM HTML W/O MATERIAL UI ELEMENTS
+// ----------------------
+// var Example = React.createClass({
+//   render: function(){
+//     return (YOUR_HTML_HERE);
+//   }
+// });
+
+// var Example = React.createClass({
+//   getInitialState: function(){
+//     return {votes: this.props.votes};
+//   },
+//   childContextTypes: {
+//     muiTheme: React.PropTypes.object
+//   },
+//   getChildContext: function() {
+//     return {
+//       muiTheme: ThemeManager.getCurrentTheme()
+//     }
+//   },
+//   render: function(){
+//     return (YOUR_HTML_HERE);
+//   }
+// });
+
+React.render(<NavBar />,
+  document.querySelector('.nav-bar')
+);
