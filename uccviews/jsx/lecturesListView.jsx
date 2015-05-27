5af182b3f01369b90b44703ea34cb6ad92d89ea8
Elvio Cavalcante <elvio@madfx.com.br>
2015-05-27 16:06:01 -0700

    Content list is displaying title and link

--- uccviews/jsx/lecturesListView.jsx
+++ uccviews/jsx/lecturesListView.jsx
@@ -1,24 +1,28 @@
 var React    = require('react'),
 	  $        = require('jquery/dist/jquery.js'),
     TopBar   = require('./template.js'),
-    mui = require('material-ui'),
-    AppBar = mui.AppBar;
+    mui      = require('material-ui'),
+    AppBar   = mui.AppBar;
 
 var fakeVideos = [
   {
     url: "https://www.youtube.com/watch?v=Jh0er2pRcq8&list=PLUPi8Qj7uZ3QpsamMg8NvqI9QWv3bK974",
+    title: "Explore MEAN Stack at 2015",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo"
   },  
   {
     url: "https://www.youtube.com/watch?v=1RMWS60gGUY&list=PLUPi8Qj7uZ3QpsamMg8NvqI9QWv3bK974&index=2",
+    title: "Building high quality services at Uber with Node.js",
     description: "ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium"
   },
   {
     url: "https://www.youtube.com/watch?v=WOVmr6CjgNw&index=3&list=PLUPi8Qj7uZ3QpsamMg8NvqI9QWv3bK974",
+    title: "Comparing Node.js Frameworks: Express, Hapi, LoopBack, Sailsjs and Meteor",
     description: "xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    url: "https://https://www.youtube.com/watch?v=FVdH9YcB3Dg&list=PLUPi8Qj7uZ3QpsamMg8NvqI9QWv3bK974&index=5",
+    url: "https://www.youtube.com/watch?v=FVdH9YcB3Dg&list=PLUPi8Qj7uZ3QpsamMg8NvqI9QWv3bK974&index=5",
+    title: "Node.js Fundamentals",
     description: " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"
   },
 ]
@@ -66,7 +70,7 @@ var LecturesList = React.createClass({
       return <ContentRow data={content} key={i}/>
     });
 		return (
-			<div>
+			<div className="container">
 				<CatalogTitle/>
         {rows}
 			</div>
@@ -79,6 +83,7 @@ var CatalogTitle = React.createClass({
 		return (
 			<div>
 				<h3>List of Contents</h3>
+
 			</div>
 		);
 	}
@@ -88,7 +93,7 @@ var ContentRow = React.createClass({
 	render: function() {
 		return (
       <div>
-        <div>{this.props.data.url}</div>
+        <div>{this.props.data.title}</div>
   			<div>{this.props.data.description}</div>
       </div>
 		);
