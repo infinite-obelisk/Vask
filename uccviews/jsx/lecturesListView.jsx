f943338ad6edc7d43f2404fd91a67c2decd1c1c1
Elvio Cavalcante <elvio@madfx.com.br>
2015-05-27 10:52:23 -0700

    Lectures List view to display content

---
+++ uccviews/jsx/lecturesListView.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var LecturesList = React.createClass({
+	render: function() {
+		return (
+			<div>Yooooo</div>
+		);
+	}
+});
+
+React.render(<LecturesList/>, document.getElementById('react-mount'));
+
