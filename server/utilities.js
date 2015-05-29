523352a8403169e957699a54ff23f4d9460fd7bd
Bryan Jennings <bryanjenningz@gmail.com>
2015-05-29 11:40:11 -0700

    client can request user information by making a GET to /getUserInfo

--- server/utilities.js
+++ server/utilities.js
@@ -1,16 +1,27 @@
 var session = require('express-session');
 
 exports.createSession = function(request, response, user) {
-    console.log('createSession');
-    return request.session.regenerate(function(err) {
-        if (err) {
-            console.log('error in creating a session', err);
-            throw err;
-        }
-        request.session.user = user;
-        console.log('created that session, redirect to home');
-        response.status(201).send(user);
-    });
+  console.log('createSession');
+  return request.session.regenerate(function(err) {
+    if (err) {
+      console.log('error in creating a session', err);
+      throw err;
+    }
+    request.session.user = user;
+    console.log('created that session, redirect to home');
+    response.status(201).send(user);
+  });
+};
+
+exports.getUserInfo = function(request, response) {
+  // If the user doesn't request any specific info that they need,
+  // we'll just send them some default information like their username
+  var neededUserInfo = request.body.neededUserInfo || ['username'];
+  console.log('session username', request.session.user.username);
+  var userInfo = neededUserInfo.map(function(info) {
+    return request.session.user[info];
+  });
+  response.send(userInfo);
 };
 
 exports.stub = function (req,res) {
