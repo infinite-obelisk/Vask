4767a28fa4dfdd656f7e11e7e68b477cac2a6573
Elvio Cavalcante <elvio@madfx.com.br>
2015-05-31 20:04:02 -0700

    Add flux architecture and scaffolding

--- uccviews/main.jsx
+++ uccviews/main.jsx
@@ -1,20 +1,15 @@
 var React = require('react'),
-    RouterMixin = require('react-mini-router').RouterMixin;
+    RouterMixin = require('react-mini-router').RouterMixin,
+    LectureList = require('./jsx/lecturesListView.jsx');
 
 var App = React.createClass({
 
     routes: {
         '/': 'lecturesList',
-        '/lists/:id/:nested*': 'viewList'
+        '/contents/:url': 'lectures'
     },
 
     mixins: [RouterMixin],
-
-    getInitialState: function(){
-    	return {
-    		lists: [{id: 001, name: 'task1'},{id: 002, name: 'task2'},{id: 003, name: 'task3'}]
-    	}
-    },
 
     render: function() {
         return <div>{this.renderCurrentRoute()}</div>;
@@ -23,20 +18,12 @@ var App = React.createClass({
     lecturesList: function() {
         return (
             <div>
-                <ul className="lists list-group">
-                {this.state.lists.map(function(list) {
-                    return (
-	                    <li className="list-group-item" key={list.id}>
-                            <a href={'/lists/' + list.id + '/'}>{list.name}</a>
-                        </li>
-                    );
-                })}
-                </ul>
+                <LectureList/>
             </div>
         );
     },
 
-    viewList: function(id) {
+    lectures: function(id) {
 	    var list = this.state.lists.reduce(function(found, list) {
 		    if (list.id == id) { return list; }
 		    return found;
