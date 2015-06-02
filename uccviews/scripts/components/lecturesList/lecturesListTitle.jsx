758a35a0b09c067e133d0c5f58466eec4320b397
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-01 17:33:38 -0700

    Add button for content uploading

--- uccviews/scripts/components/lecturesList/lecturesListTitle.jsx
+++ uccviews/scripts/components/lecturesList/lecturesListTitle.jsx
@@ -1,15 +1,27 @@
 'use strict'
 
-var React = require('react');
+var React = require('react'),
+	mui   = require('material-ui'),
+	RaisedButton = mui.RaisedButton,
+	MaterialMixin = require('../../mixins/material-ui.js');
 
 var LectureListTitle = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <h3>List of Contents</h3>
-      </div>
-    );
-  }
+
+  mixins: [MaterialMixin],
+
+	render: function() {
+		return (
+		  <div className="lectureTitleBox row">
+		  	<div className="col-lg-6 col-md-6 col-sm-6">
+		  	  <h3>List of Contents</h3>
+		  	</div>
+		  	
+		  	<div className="col-lg-6 col-md-6 col-sm-6" style={{"paddingTop": "10px"}}>
+		  	  <RaisedButton label="+ Add Content" secondary={true} style={{"float": "right"}}/>
+		  	</div>
+		  </div>
+		);
+	}
 });
 
 module.exports = LectureListTitle;
