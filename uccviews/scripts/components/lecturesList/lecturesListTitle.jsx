2e7a17f80d7f695f49631eec10734287140a1eca
Elvio Cavalcante <elvio@madfx.com.br>
2015-06-01 19:53:25 -0700

    Add dialog box when click on Add Content button

--- uccviews/scripts/components/lecturesList/lecturesListTitle.jsx
+++ uccviews/scripts/components/lecturesList/lecturesListTitle.jsx
@@ -1,8 +1,9 @@
 'use strict'
 
-var React = require('react'),
-	mui   = require('material-ui'),
-	RaisedButton = mui.RaisedButton,
+var React         = require('react'),
+	mui           = require('material-ui'),
+	RaisedButton  = mui.RaisedButton,
+	AddContent    = require('./addContent.jsx'),
 	MaterialMixin = require('../../mixins/material-ui.js');
 
 var LectureListTitle = React.createClass({
@@ -17,7 +18,7 @@ var LectureListTitle = React.createClass({
 		  	</div>
 		  	
 		  	<div className="col-lg-6 col-md-6 col-sm-6" style={{"paddingTop": "10px"}}>
-		  	  <RaisedButton label="+ Add Content" secondary={true} style={{"float": "right"}}/>
+		  	  <AddContent/>
 		  	</div>
 		  </div>
 		);
