8e625b7658d876b727547c79f6b3032ded990877
Kirstin "Kir" Jarchow <me@kirstinjarchow.com>
2015-06-04 16:47:19 -0700

    Answer Form fixes

--- uccviews/scripts/components/lectureView/viewQuestionElements/answerForm.jsx
+++ uccviews/scripts/components/lectureView/viewQuestionElements/answerForm.jsx
@@ -6,16 +6,43 @@ var React = require('react'),
 
 var AnswerForm = React.createClass({
   mixins: [MaterialMixin],
+  componentDidMount: function(){
+    $(".answer-form>div>textarea").parent().appendTo($(".answer-form"));
+  },
+  addAnswer: function (questionId, answerText) {
+    $.ajax({
+      url: "/addanswer",
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({
+        text : answerText,
+        _id : questionId
+      }),
+      statusCode: {
+        201: function (data) {
+          console.log('win');
+          console.log(data);
+          //self.clearForm();
+          //self.closeDialog();
+        },
+        500: function (err) {
+          console.log('lose')
+        }
+      }
+    });
+  },
   submitAnswer: function(){
     var answerText = this.refs.answerText.getValue();
     var questionId = this.props.questionId;
-    addAnswer(questionId, answerText);
+    this.addAnswer(questionId, answerText);
+    $(".answer-form>div>textarea").val("");
   },
   render: function(){
     return (<div>
               <TextField
                 hintText="Type your answer here..."
                 ref="answerText"
+                className="answer-form"
                 multiLine={true}
                 style={{"width": "95%"}}/>
               <FlatButton
