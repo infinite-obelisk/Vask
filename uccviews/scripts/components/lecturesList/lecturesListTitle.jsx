f37aceb1ebaf8fdaec56ac9ebe91717fca4cbbdc
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-06-05 11:37:21 -0700

    Add search bar in the lecture list view

--- uccviews/scripts/components/lecturesList/lecturesListTitle.jsx
+++ uccviews/scripts/components/lecturesList/lecturesListTitle.jsx
@@ -4,7 +4,8 @@ var React         = require('react'),
 	mui           = require('material-ui'),
 	RaisedButton  = mui.RaisedButton,
 	AddLecture    = require('./addLecture.jsx'),
-	MaterialMixin = require('../../mixins/material-ui.js');
+	MaterialMixin = require('../../mixins/material-ui.js'),
+	TextField     = mui.TextField;
 
 var LectureListTitle = React.createClass({
 
@@ -12,14 +13,21 @@ var LectureListTitle = React.createClass({
 
 	render: function() {
 		return (
-		  <div className="lectureTitleBox row">
-		  	<div className="col-lg-6 col-md-6 col-sm-6">
-		  	  <h3>Lectures</h3>
-		  	</div>
-		  	
-		  	<div className="col-lg-6 col-md-6 col-sm-6" style={{"paddingTop": "10px"}}>
-		  	  <AddLecture />
-		  	</div>
+		  <div className="row lectureTitleBox">
+		    <div className="col-lg-3">
+		      <h3>Lectures</h3>
+		    </div>
+		    <div className="col-lg-6 searchBar">
+		      <div className="icon-search-box">
+		        <svg style={{"width":"29px","height":"29px"}} className="icon-search" viewBox="0 0 24 24">
+		            <path fill="#999" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+		        </svg>
+		      </div>
+		      <input type="text" placeholder="Search for lectures.."/>
+		    </div>
+		    <div className="col-lg-3 ">
+		      <AddLecture />
+		    </div>
 		  </div>
 		);
 	}
